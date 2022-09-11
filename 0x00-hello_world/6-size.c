05b5b5213d7cc173281df20bf547e41c2cdf9b7c
Madukavic <madukavictor2300@gmail.com>
2022-09-11 11:10:38 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ char a;
 int b;
 long int c;
 long long int d;
-float e;i
+float e;
 
 printf("Size of a char: %d byte(s)\n", sizeof(a));
 printf("Size of an int: %d byte(s)\n", sizeof(b));
@@ -20,4 +20,3 @@ printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 printf("Size of a float: %d byte(s)\n", sizeof(e));
 return (0);
 }
-
