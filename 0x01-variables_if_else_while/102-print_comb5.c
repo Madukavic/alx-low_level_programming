0550690459b1b3961c0831ccdfca37d277d2d7d1
Madukavic <madukavictor2300@gmail.com>
2022-09-12 15:30:04 -0700

    print combs 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,26 +10,23 @@ int main(void)
 {
 int i;
 int p;
-for(i = 0 ; i < 100 ; i++)
+for (i = 0 ; i < 100 ; i++)
+{	       
+for (p = 0 ; p < 100 ; p++)
 {
-	       
-for(p = 0 ; p < 100 ; p++)
-{
-if(p > i)
+if (p > i)
 {
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
 putchar(' ');
 putchar((p / 10) + '0');
 putchar((p % 10) + '0');
-if(i != 98)
+if (i != 98)
 {
 putchar(',');
 putchar(' ');
 }
-
 }    
-
 }   
 }
 putchar('\n');
