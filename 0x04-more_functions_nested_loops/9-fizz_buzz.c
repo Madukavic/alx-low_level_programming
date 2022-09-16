c66bf4ba199aba748bc085f32a15950cbb4a6f80
Madukavic <madukavictor2300@gmail.com>
2022-09-16 16:37:00 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,28 +6,27 @@
  */
 int main(void)
 {
-int h = 1;
+        int h = 1;
 
-while (h <= 100)
-{
-if (h % 3 == 0 && h % 5 == 0)
-
-printf("Fizzbuzz ");
-else if (h % 5 == 0)
-{
-if (h == 100)
-{
-printf("Buzz");
-printf("\n");
-}
-else
-printf("Buzz ");
-}
-else if (h % 3 == 0)
-printf("Fizz ");
-else
-printf("%d ", h);
-h++
-}
+        while (h <= 100)
+        {
+                if (h % 3 == 0 && h % 5 == 0)
+                        printf("FizzBuzz ");
+                else if (h % 5 == 0)
+                {
+                        if (h == 100)
+                        {
+                                printf("Buzz");
+                                printf("\n");
+                        }
+                        else
+                                printf("Buzz ");
+                }
+                else if (h % 3 == 0)
+                        printf("Fizz ");
+                else
+                        printf("%d ", h);
+         h++;
+         }
 return (0);
 }
