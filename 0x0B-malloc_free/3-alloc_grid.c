981ce128d73ae39f69ce55ff7a515a7aece775ab
Madukavic <madukavictor2300@gmail.com>
2022-10-04 15:47:34 -0700

    3-alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,9 +19,9 @@ int **alloc_grid(int width, int height)
 	if (array != NULL)
 	{
 		for (; i < height; i++)
-		
+
 		{
-			array[i] = (int *) malloc(sizeof(int) *width);
+			array[i] = (int *) malloc(sizeof(int) * width);
 			if (array[i] != NULL)
 			{
 				for (j = 0; j < width; j++)
