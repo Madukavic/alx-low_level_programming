1715b463475650e92ac18106bfe61bf829346dd8
Madukavic <madukavictor2300@gmail.com>
2022-10-07 17:53:42 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -60,6 +60,7 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 	       return (NULL);
 	}
         return (dest);
+}
 
 /**
  * check_for_digits - checks the arguments to ensure they are digits
